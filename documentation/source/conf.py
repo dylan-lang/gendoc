c684b4426b26d7982ef4203866fb0cbfdfbf4426
Carl Gay <carlgay@gmail.com>
2024-05-14 11:08:14 -0400

    Add documentation scaffolding
    This won't be actual documentation for gendoc (the README is enough). Rather,
    it provides a directory and index.rst file in which to generate package docs
    and the Makefile to go with it.

---
+++ documentation/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Dylan Package Documentation'
+copyright = '2024, Dylan Hackers'
+author = 'Dylan Hackers'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../_packages/sphinx-extensions/current/src/sphinxcontrib'))
+extensions = [
+    'dylan.domain',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.intersphinx',
+]
+primary_domain = 'dylan'
+exclude_patterns = [
+    '**/README.*',
+]
+show_authors = True
+templates_path = ['_templates']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'             # https://pradyunsg.me/furo/customisation/
